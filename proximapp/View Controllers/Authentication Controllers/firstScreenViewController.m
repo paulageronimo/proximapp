5b6388fcf4b2094d33f1d888cedbff67810d5400
Paula Letícia Gerónimo <paulagero182@gmail.com>
2021-07-24 23:22:03 -0400

    Created initial view controller, welcoming screen first view upon going into the app.

---
+++ proximapp/View Controllers/Authentication Controllers/firstScreenViewController.m
@@ -0,0 +1,38 @@
+//
+//  firstScreenViewController.m
+//  proximapp
+//
+//  Created by Paula Leticia Geronimo on 7/24/21.
+//
+
+#import "firstScreenViewController.h"
+#import "BackgroundVideo.h"
+
+@interface firstScreenViewController ()
+@property (strong, nonatomic) BackgroundVideo *backgroundVideo;
+@property (weak, nonatomic) IBOutlet UIButton *signupButton;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+
+@end
+
+@implementation firstScreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.backgroundVideo = [[BackgroundVideo alloc] initOnViewController:self withVideoURL:@"login-video.mp4"];
+    [self.backgroundVideo setUpBackground];
+    _signupButton.layer.cornerRadius = 12.0;
+    _loginButton.layer.cornerRadius = 12.0;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
