fd6cf9f1284893b7bbb1fa841c1642ca4ba6c32a
Paula Letícia Gerónimo <paulagero182@gmail.com>
2021-07-24 23:22:41 -0400

    Welcoming screen's respective .h file

---
+++ proximapp/View Controllers/Authentication Controllers/firstScreenViewController.h
@@ -0,0 +1,16 @@
+//
+//  firstScreenViewController.h
+//  proximapp
+//
+//  Created by Paula Leticia Geronimo on 7/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface firstScreenViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
