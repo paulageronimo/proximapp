a7013def3235903f6b4cd0af4afd0dc96c8a5f94
Paula Letícia Gerónimo <paulagero182@gmail.com>
2021-07-25 23:34:05 -0400

    Created VC for product search list

---
+++ proximapp/View Controllers/User Viewing View Controllers/SearchProductViewController.h
@@ -0,0 +1,16 @@
+//
+//  SearchProductViewController.h
+//  proximapp
+//
+//  Created by Paula Leticia Geronimo on 7/25/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SearchProductViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ proximapp/View Controllers/User Viewing View Controllers/SearchProductViewController.m
@@ -0,0 +1,31 @@
+//
+//  SearchProductViewController.m
+//  proximapp
+//
+//  Created by Paula Leticia Geronimo on 7/25/21.
+//
+
+#import "SearchProductViewController.h"
+
+@interface SearchProductViewController ()
+
+@end
+
+@implementation SearchProductViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
