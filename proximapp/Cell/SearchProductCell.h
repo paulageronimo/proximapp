5b3d2e4a333d386a89e309e76bb35236302cfcec
Paula Letícia Gerónimo <paulagero182@gmail.com>
2021-07-28 16:13:36 -0400

    Search related edits, updated cell and storyboard

--- proximapp/Cell/SearchProductCell.h
+++ proximapp/Cell/SearchProductCell.h
@@ -11,7 +11,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SearchProductCell : UITableViewCell
 
-- (void)updateWithLocation:(NSDictionary *)location;
+- (void)updateWithDetails:(NSDictionary *)post;
 
 @end
 
